7f6f85e0764674c035fd7b7d136452e5790cb180
71619997a <gmarks@stuy.edu>
2016-10-29 17:40:26 -0400

    Add error check and ctime

---
+++ stat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main() {
+  char filename[20] = "test.txt";
+  struct stat* info = (struct stat*)malloc(sizeof(struct stat));
+  if(stat(filename, info) == -1) {
+      printf(strerror(errno));
+      return errno;
+  };
+  printf("Size: %d", info->st_size);
+  printf("\nMode: %o", info->st_mode);
+  printf("\nTime: %s", ctime(&(info->st_atime)));
+  free(info);
+}
